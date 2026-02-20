bacfd75d02c40d3146f857576e87858b3d43a1a3
uss-stargazer <163490937+uss-stargazer@users.noreply.github.com>
2026-02-20 02:37:51 -0500

    wip feat: better error handling

--- src/App.tsx
+++ src/App.tsx
@@ -14,9 +14,11 @@ import {
 import ClientForm from "./components/ClientForm";
 import useClients, { ClientProvider } from "./hooks/useClients";
 import type { Client } from "./modules/clients";
-import { useRef, useState, type Ref } from "react";
-import { Add, Dashboard, ExpandMore, Settings } from "@mui/icons-material";
+import { useRef, useState } from "react";
+import { Add, ExpandMore, Settings } from "@mui/icons-material";
 import Card from "./components/Card";
+import Dashboard from "./components/Dashboard";
+import { ErrorBoundary } from "react-error-boundary";
 
 function ClientEditor() {
   const clientData = useClients();
@@ -106,6 +108,46 @@ const theme = createTheme({
   typography: { fontFamily: "'Fira Mono', monospace" },
 });
 
+function ErrorFallback({ error }: { error: unknown }) {
+  const [showError, setShowError] = useState<boolean>(false);
+  return (
+    <Box
+      sx={{
+        width: "100vw",
+        height: "100vh",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        gap: 1,
+      }}
+    >
+      <Typography variant="h4" color="warning">
+        :[
+      </Typography>
+      <Typography align="center" maxWidth="50%">
+        The right code in the wrong place can make all the difference in the
+        world...
+      </Typography>
+
+      <Button
+        size="small"
+        color="error"
+        onClick={() => setShowError(!showError)}
+      >
+        {showError ? "Hide" : "Show"} error
+      </Button>
+      {showError && (
+        <Typography variant="caption" align="center" maxWidth="75%">
+          {error instanceof Error
+            ? error.message
+            : JSON.stringify(error, undefined, "  ")}
+        </Typography>
+      )}
+    </Box>
+  );
+}
+
 function App() {
   const editClientsRef = useRef<HTMLDivElement | null>(null);
   const [editClientsExpanded, setEditClientsExpanded] =
@@ -121,8 +163,8 @@ function App() {
   };
 
   return (
-    <ThemeProvider theme={theme}>
-      <ClientProvider>
+    <ErrorBoundary FallbackComponent={ErrorFallback}>
+      <ThemeProvider theme={theme}>
         <Box sx={{ display: "flex", flexDirection: "column" }}>
           <Box>
             <AppBar position="static">
@@ -137,36 +179,38 @@ function App() {
             </AppBar>
           </Box>
 
-          <Box
-            sx={{
-              flexGrow: 1,
-              p: "1rem",
-              display: "flex",
-              flexDirection: "column",
-              gap: "1rem",
-            }}
-          >
-            <Dashboard />
-
-            <Accordion
-              ref={editClientsRef}
-              expanded={editClientsExpanded}
-              onChange={(_, expanded) => setEditClientsExpanded(expanded)}
+          <ClientProvider>
+            <Box
+              sx={{
+                flexGrow: 1,
+                p: "1rem",
+                display: "flex",
+                flexDirection: "column",
+                gap: "1rem",
+              }}
             >
-              <AccordionSummary expandIcon={<ExpandMore />}>
-                <Box display="flex" gap="1rem">
-                  <Settings />
-                  <Typography component="span">Edit clients</Typography>
-                </Box>
-              </AccordionSummary>
-              <AccordionDetails>
-                <ClientEditor />
-              </AccordionDetails>
-            </Accordion>
-          </Box>
+              <Dashboard />
+
+              <Accordion
+                ref={editClientsRef}
+                expanded={editClientsExpanded}
+                onChange={(_, expanded) => setEditClientsExpanded(expanded)}
+              >
+                <AccordionSummary expandIcon={<ExpandMore />}>
+                  <Box display="flex" gap="1rem">
+                    <Settings />
+                    <Typography component="span">Edit clients</Typography>
+                  </Box>
+                </AccordionSummary>
+                <AccordionDetails>
+                  <ClientEditor />
+                </AccordionDetails>
+              </Accordion>
+            </Box>
+          </ClientProvider>
         </Box>
-      </ClientProvider>
-    </ThemeProvider>
+      </ThemeProvider>
+    </ErrorBoundary>
   );
 }
 
--- src/hooks/useClients.tsx
+++ src/hooks/useClients.tsx
@@ -1,4 +1,4 @@
-import z from "zod";
+import z, { ZodError } from "zod";
 import { ClientSchema, type Client } from "../modules/clients";
 import {
   createContext,
@@ -7,7 +7,7 @@ import {
   useState,
   type PropsWithChildren,
 } from "react";
-import { ErrorBoundary } from "react-error-boundary";
+import { Box, Button, Divider, Typography } from "@mui/material";
 
 const ClientArraySchema = z.array(ClientSchema);
 
@@ -18,78 +18,136 @@ type ClientContextType = {
 };
 const ClientContext = createContext<ClientContextType | null>(null);
 
-type ClientProviderProps = PropsWithChildren<{
-  storageKey: string;
-  defaultClients: Client[];
-}>;
-
-function ClientProviderNoCatch({
+export function ClientProvider({
   children,
-  storageKey,
-  defaultClients,
-}: ClientProviderProps) {
+  storageKey = "clients",
+  defaultClients = [],
+}: PropsWithChildren<
+  Partial<{
+    storageKey: string;
+    defaultClients: Client[];
+  }>
+>) {
   const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string | null>(null);
+  const [showError, setShowError] = useState<boolean>(false);
   const [clients, setClients] = useState<Client[]>(defaultClients);
 
-  useEffect(() => {
-    const raw = localStorage.getItem(storageKey);
-    if (raw !== null) {
-      const clients = ClientArraySchema.parse(JSON.parse(raw));
-      if (new Set(clients.map((c) => c.name)).size !== clients.length)
-        throw new Error("Invalid stored clients: has duplicate client names");
-      setClients(clients);
-    }
-    setIsLoading(false);
-  }, []);
-
-  const clientsObj: ClientContextType = {
-    isLoading,
-    clients,
-    setClients(updated) {
-      localStorage.setItem(storageKey, JSON.stringify(updated));
-      setClients(updated);
-    },
+  const loadData = () => {
+    setError(null);
+    setIsLoading(true);
+    new Promise<void>((resolve) => {
+      const raw = localStorage.getItem(storageKey);
+      if (raw !== null) {
+        try {
+          const clients = ClientArraySchema.parse(JSON.parse(raw));
+          if (new Set(clients.map((c) => c.name)).size !== clients.length)
+            throw new SyntaxError(
+              "Invalid stored clients: has duplicate client names",
+            );
+          setClients(clients);
+        } catch (error) {
+          if (error instanceof ZodError) {
+            setError(z.prettifyError(error));
+          } else if (error instanceof SyntaxError) {
+            setError(error.message);
+          } else throw error;
+        }
+      }
+      resolve();
+    }).finally(() => {
+      console.log("set loading false");
+      setIsLoading(false);
+    });
   };
 
-  return (
-    <ClientContext.Provider value={clientsObj}>
+  useEffect(loadData, []);
+
+  return error ? (
+    <Box
+      sx={{
+        width: "100vw",
+        height: "100vh",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        gap: 1,
+      }}
+    >
+      <Typography>Data stored locally is broken! :[</Typography>
+      <Button
+        variant="outlined"
+        color="warning"
+        onClick={() => {
+          localStorage.removeItem(storageKey);
+          loadData();
+        }}
+      >
+        Reset local storage
+      </Button>
+
+      <Button
+        size="small"
+        color="error"
+        onClick={() => setShowError(!showError)}
+      >
+        {showError ? "Hide" : "Show"} error
+      </Button>
+      {showError && (
+        <Typography variant="caption" align="center" maxWidth="75%">
+          {error}
+        </Typography>
+      )}
+    </Box>
+  ) : (
+    <ClientContext.Provider
+      value={{
+        isLoading,
+        clients,
+        setClients(updated) {
+          localStorage.setItem(storageKey, JSON.stringify(updated));
+          setClients(updated);
+        },
+      }}
+    >
       {children}
     </ClientContext.Provider>
   );
 }
 
-export const ClientProvider = ({
-  children,
-  storageKey = "clients",
-  defaultClients = [],
-}: Partial<ClientProviderProps>) => (
-  <ErrorBoundary
-    FallbackComponent={({ error, resetErrorBoundary }) => (
-      <>
-        <h4>
-          Data stored locally is broken! Either fix it or{" "}
-          <a
-            onClick={() => {
-              localStorage.removeItem(storageKey);
-              resetErrorBoundary();
-            }}
-          >
-            reset localStorage
-          </a>
-          .
-        </h4>
-        {error instanceof Error && <span>{error.message}</span>}
-      </>
-    )}
-  >
-    <ClientProviderNoCatch
-      storageKey={storageKey}
-      defaultClients={defaultClients}
-    >
-      {children}
-    </ClientProviderNoCatch>
-  </ErrorBoundary>
-);
+// export const ClientProvider = ({
+//   children,
+//   storageKey = "clients",
+//   defaultClients = [],
+// }: Partial<ClientProviderProps>) => (
+//   <ErrorBoundary
+//     FallbackComponent={({ error, resetErrorBoundary }) => (
+//       <>
+//         <h4>
+//           Data stored locally is broken! Either fix it or{" "}
+//           <a
+//             onClick={() => {
+//               localStorage.removeItem(storageKey);
+//               resetErrorBoundary();
+//             }}
+//           >
+//             reset localStorage
+//           </a>
+//           .
+//         </h4>
+//         {error instanceof Error && <span>{error.message}</span>}
+//       </>
+//     )}
+//   >
+//     <ClientProviderNoCatch
+//       storageKey={storageKey}
+//       defaultClients={defaultClients}
+//     >
+//       {children}
+//     </ClientProviderNoCatch>
+//   </ErrorBoundary>
+// );
 
 const useClients = (): ClientContextType => {
   const clients = useContext(ClientContext);
