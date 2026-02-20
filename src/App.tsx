ff237bb2675e40a222ccfc5886fa0483bd79fa94
uss-stargazer <163490937+uss-stargazer@users.noreply.github.com>
2026-02-20 13:35:33 -0500

    feat: ClientEditor as bottom drawer

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,4 @@
 import {
-  Accordion,
-  AccordionDetails,
-  AccordionSummary,
   AppBar,
   Box,
   Button,
@@ -11,89 +8,12 @@ import {
   Toolbar,
   Typography,
 } from "@mui/material";
-import ClientForm from "./components/ClientForm";
-import useClients, { ClientProvider } from "./hooks/useClients";
-import type { Client } from "./modules/clients";
 import { useRef, useState } from "react";
-import { Add, ExpandMore, Settings } from "@mui/icons-material";
-import Card from "./components/Card";
+import { Settings } from "@mui/icons-material";
 import Dashboard from "./components/Dashboard";
 import { ErrorBoundary } from "react-error-boundary";
-
-function ClientEditor() {
-  const clientData = useClients();
-  const [stagedClient, setStagedClient] = useState<Partial<Client> | null>(
-    null,
-  );
-  if (clientData.isLoading) return <Button loading variant="outlined" />;
-
-  const clientNames = clientData.clients.map((c) => c.name);
-  const addClient = (client: Client) => {
-    if (clientNames.includes(client.name))
-      throw new Error("Add client: client name must be unique");
-    clientData.setClients([...clientData.clients, client]);
-  };
-  const removeClient = (clientName: string) => {
-    if (clientData.clients.some((c) => c.name === clientName))
-      clientData.setClients(
-        clientData.clients.filter((c) => c.name !== clientName),
-      );
-  };
-  const updateClient = (ogName: string, updated: Client) => {
-    if (ogName !== updateClient.name && clientNames.includes(updateClient.name))
-      throw new Error("Update client: new client name must be unique");
-    clientData.setClients([
-      ...clientData.clients.filter((c) => c.name !== ogName),
-      updated,
-    ]);
-  };
-
-  return (
-    <Box
-      sx={{
-        display: "flex",
-        flexWrap: "wrap",
-        gap: 2,
-      }}
-    >
-      {[
-        ...clientData.clients.map((client) => (
-          <ClientForm
-            key={client.name}
-            client={client}
-            invalidNames={clientNames.filter((c) => c !== client.name)}
-            submitText="Update"
-            onSubmit={(updated) => updateClient(client.name, updated)}
-            otherButtons={[
-              { label: "Remove", onClick: () => removeClient(client.name) },
-            ]}
-          />
-        )),
-        stagedClient ? (
-          <ClientForm
-            key="staged"
-            client={stagedClient}
-            invalidNames={clientNames}
-            submitText="Add"
-            onSubmit={(client) => {
-              addClient(client);
-              setStagedClient(null);
-            }}
-            otherButtons={[
-              { label: "Cancel", onClick: () => setStagedClient(null) },
-            ]}
-          />
-        ) : (
-          <Card fullWidth sx={{ display: "flex" }}>
-            <Button sx={{ flexGrow: 1 }} onClick={() => setStagedClient({})}>
-              <Add />
-            </Button>
-          </Card>
-        ),
-      ]}
-    </Box>
-  );
-}
+import ClientEditor from "./components/ClientEditor";
+import { ClientProvider } from "./hooks/useClients";
 
 const theme = createTheme({
   palette: {
@@ -150,11 +70,10 @@ function ErrorFallback({ error }: { error: unknown }) {
 
 function App() {
   const editClientsRef = useRef<HTMLDivElement | null>(null);
-  const [editClientsExpanded, setEditClientsExpanded] =
-    useState<boolean>(false);
+  const [editClientsOpen, setEditClientsOpen] = useState<boolean>(false);
   const scrollToEditClients = () => {
     if (editClientsRef.current) {
-      setEditClientsExpanded(true);
+      setEditClientsOpen(true);
       editClientsRef.current.scrollIntoView({
         behavior: "smooth",
         block: "start",
@@ -190,22 +109,11 @@ function App() {
               }}
             >
               <Dashboard />
-
-              <Accordion
+              <ClientEditor
+                isOpen={editClientsOpen}
+                setIsOpen={setEditClientsOpen}
                 ref={editClientsRef}
-                expanded={editClientsExpanded}
-                onChange={(_, expanded) => setEditClientsExpanded(expanded)}
-              >
-                <AccordionSummary expandIcon={<ExpandMore />}>
-                  <Box display="flex" gap="1rem">
-                    <Settings />
-                    <Typography component="span">Edit clients</Typography>
-                  </Box>
-                </AccordionSummary>
-                <AccordionDetails>
-                  <ClientEditor />
-                </AccordionDetails>
-              </Accordion>
+              />
             </Box>
           </ClientProvider>
         </Box>
