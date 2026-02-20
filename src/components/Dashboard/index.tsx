687470f51770181d917301ff47b914ff5508ebbd
uss-stargazer <163490937+uss-stargazer@users.noreply.github.com>
2026-02-20 11:46:43 -0500

    feat: ExpectedVsActual dashboard panel

--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -1,12 +1,62 @@
+import { Box, Button, Typography } from "@mui/material";
+import ExpectedVsActual from "./ExpectedVsActual";
+import useClients from "../../hooks/useClients";
+import { Info } from "@mui/icons-material";
+import Card from "../Card";
+
 /**
- * Dashboard features:
- * - Expected vs actual over time period
- *    - Calculate expected at average of all rates, min, and max
- *
  * @todo Time distribution graph accross clients (top n)
  */
 function Dashboard() {
-  return <div></div>;
+  const { clients, isLoading } = useClients();
+
+  return (
+    <Box
+      sx={{
+        minHeight: "10rem",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        gap: 1,
+      }}
+    >
+      {isLoading ? (
+        <Button loading />
+      ) : clients.length === 0 ? (
+        <>
+          <Info />
+          <Typography textAlign="center">
+            Add a client to view your time dashboard.
+          </Typography>
+        </>
+      ) : (
+        <>
+          {[<ExpectedVsActual />].map((el, idx) => (
+            <Card
+              key={idx}
+              sx={{
+                display: "flex",
+                flexDirection: {
+                  xs: "column",
+                  sm: "column",
+                  md: "row",
+                },
+                justifyContent: "center",
+                alignItems: "center",
+                gap: 1,
+                border: "1px solid",
+                borderColor: "primary.main",
+                maxWidth: undefined,
+              }}
+            >
+              {el}
+            </Card>
+          ))}
+        </>
+      )}
+    </Box>
+  );
 }
 
 export default Dashboard;
