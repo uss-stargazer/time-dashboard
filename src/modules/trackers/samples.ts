0bb9cb463a2fcf0c378f3b5e03627876148eda24
uss-stargazer <163490937+uss-stargazer@users.noreply.github.com>
2026-02-21 16:05:52 -0500

    refactor: setup for production trackers

---
+++ src/modules/trackers/samples.ts
@@ -0,0 +1,19 @@
+import z from "zod";
+import { makeTracker, TrackerError } from ".";
+
+export const sample1 = makeTracker({
+  prettyName: "Sample Tracker 1",
+  clientDataSchema: z.object({ apiToken: z.string() }),
+  getBillableHours: async (to, from, client) => {
+    if (client.apiToken.length !== 5)
+      throw new TrackerError("sample1", "invalid apiToken");
+    return (from.getSeconds() - to.getSeconds()) / 3600;
+  },
+});
+
+export const sample2 = makeTracker({
+  prettyName: "Sample Tracker 2",
+  clientDataSchema: z.object({ uuid: z.uuid(), id: z.string().length(3) }),
+  getBillableHours: async (to, from, client) =>
+    ((from.getSeconds() - to.getSeconds()) / 3600) * client.id.length,
+});
