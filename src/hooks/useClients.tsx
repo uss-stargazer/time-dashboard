90f922e1d4966210f9da8137397190b2f377637c
uss-stargazer <163490937+uss-stargazer@users.noreply.github.com>
2026-02-25 14:33:15 -0500

    feat: ClientEditor on broken local storage page

--- src/hooks/useClients.tsx
+++ src/hooks/useClients.tsx
@@ -9,6 +9,7 @@ import {
 } from "react";
 import { Box, Button, Typography } from "@mui/material";
 import trackers from "../modules/trackers";
+import ClientEditor from "../components/ClientEditor";
 
 const ClientArraySchema = z.array(ClientSchema);
 
@@ -33,6 +34,7 @@ export function ClientProvider({
   const [error, setError] = useState<string | null>(null);
   const [showError, setShowError] = useState<boolean>(false);
   const [clients, setClients] = useState<Client[]>(defaultClients);
+  const [clientEditorOpen, setClientEditorOpen] = useState<boolean>(false);
 
   const loadData = () => {
     setError(null);
@@ -49,30 +51,27 @@ export function ClientProvider({
 
           Promise.all(
             clients.map((client) =>
-              (async () => {
+              (async (): Promise<{ client: Client; error?: string }> => {
+                let error: string | undefined = undefined;
                 const tracker = trackers[client.tracker.name];
                 if (tracker.computed)
-                  client.tracker.computed = await tracker.computed.compute(
-                    client.name,
-                    // @ts-expect-error TODO: better way. Like I said elsewhere, I'm tired trying to get ts to mesh
-                    client.tracker.data,
-                  );
-                return client;
+                  client.tracker.computed = await tracker.computed
+                    .compute(
+                      client.name,
+                      // @ts-expect-error TODO: better way. Like I said elsewhere, I'm tired trying to get ts to mesh
+                      client.tracker.data,
+                    )
+                    .catch((e) => {
+                      error =
+                        e instanceof Error ? e.message : JSON.stringify(e);
+                    });
+                return { client, error };
               })(),
             ),
-          )
-            .catch((error) => {
-              setError(
-                error instanceof Error ? error.message : JSON.stringify(error),
-              );
-              return undefined;
-            })
-            .then((clients) => {
-              if (clients) {
-                setClients(clients);
-                setError(null);
-              }
-            });
+          ).then((clients) => {
+            setClients(clients.map((c) => c.client));
+            setError(clients.find((c) => c.error !== undefined)?.error ?? null);
+          });
         } catch (error) {
           if (error instanceof ZodError) {
             setError(z.prettifyError(error));
@@ -86,6 +85,11 @@ export function ClientProvider({
   };
 
   useEffect(loadData, []);
+
+  const setClientsWStorage: ClientContextType["setClients"] = (updated) => {
+    localStorage.setItem(storageKey, JSON.stringify(updated));
+    setClients(updated);
+  };
 
   return error ? (
     <Box
@@ -123,16 +127,28 @@ export function ClientProvider({
           {error}
         </Typography>
       )}
+
+      <ClientContext.Provider
+        value={{
+          isLoading,
+          clients,
+          setClients: setClientsWStorage,
+        }}
+      >
+        {clients.length > 0 && (
+          <ClientEditor
+            isOpen={clientEditorOpen}
+            setIsOpen={setClientEditorOpen}
+          />
+        )}
+      </ClientContext.Provider>
     </Box>
   ) : (
     <ClientContext.Provider
       value={{
         isLoading,
         clients,
-        setClients(updated) {
-          localStorage.setItem(storageKey, JSON.stringify(updated));
-          setClients(updated);
-        },
+        setClients: setClientsWStorage,
       }}
     >
       {children}
