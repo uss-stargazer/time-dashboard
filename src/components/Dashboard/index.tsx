73c4d37ffb02b97524a693d546cd5dbb88b9ccc5
uss-stargazer <163490937+uss-stargazer@users.noreply.github.com>
2026-06-24 16:46:24 -0400

    style: move date range selector back to main dashboard

--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -1,6 +1,8 @@
 import {
   Box,
   Button,
+  Radio,
+  Stack,
   Typography,
   type BoxProps,
   type SxProps,
@@ -9,12 +11,71 @@ import ExpectedVsActual from './panels/ExpectedVsActual';
 import useSettings from '../../hooks/useSettings';
 import { Error as ErrorIcon, Info } from '@mui/icons-material';
 import Monthly from './panels/Monthly';
+import { DatePicker, LocalizationProvider } from '@mui/x-date-pickers';
+import dayjs, { Dayjs } from 'dayjs';
+import { useState } from 'react';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { type ReactElement } from 'react';
 import trackers from '../../modules/trackers';
 import Card from '../Card';
 import useDashboardState, {
   DashboardStateProvider,
 } from './hooks/useDashboardState';
+
+function DateInput({
+  label,
+  value,
+  onChange,
+  validate,
+  presets,
+}: {
+  label: string;
+  value: Dayjs;
+  onChange: (newDate: Dayjs) => void;
+  validate: (newDate: Dayjs) => void | string;
+  presets?: { label: string; onClick: () => void }[];
+}) {
+  const [error, setError] = useState<string | null>(null);
+  return (
+    <Box>
+      <DatePicker
+        label={label}
+        value={value}
+        onChange={(value, { validationError }) => {
+          if (value && value.isValid() && !validationError) {
+            const error = validate(value);
+            if (typeof error === 'string') setError(error);
+            else {
+              setError(null);
+              onChange(value);
+            }
+          }
+        }}
+        disableFuture
+      />
+      {error && (
+        <Typography variant='caption' color='error'>
+          {error}
+        </Typography>
+      )}
+      {presets && (
+        <Box sx={{ display: 'flex', alignItems: 'center', gap: 0.5 }}>
+          {presets.map((preset) => (
+            <Box key={preset.label}>
+              <Radio
+                size='small'
+                checked={false}
+                onClick={preset.onClick}
+                sx={{ p: 0.5 }}
+              />
+              <Typography variant='caption'>{preset.label}</Typography>
+            </Box>
+          ))}
+        </Box>
+      )}
+    </Box>
+  );
+}
 
 function DashboardPanel({
   name,
@@ -108,6 +169,10 @@ function Dashboard({ sx }: { sx?: SxProps }) {
       </Box>
     );
 
+  const [startDate, endDate] = settings.dateRange;
+  const setStartDate = (d: Dayjs) => settings.setDateRange([d, endDate]);
+  const setEndDate = (d: Dayjs) => settings.setDateRange([startDate, d]);
+
   return (
     <DashboardStateProvider>
       <Box
@@ -118,6 +183,40 @@ function Dashboard({ sx }: { sx?: SxProps }) {
           ...sx,
         }}
       >
+        <Stack gap={1}>
+          <LocalizationProvider dateAdapter={AdapterDayjs}>
+            <DateInput
+              label='From'
+              value={startDate}
+              onChange={setStartDate}
+              validate={(startDate) => {
+                if (startDate.isAfter(endDate))
+                  return 'Start must be before end!';
+              }}
+              presets={[
+                {
+                  label: 'month',
+                  onClick: () => setStartDate(endDate.startOf('month')),
+                },
+                {
+                  label: 'year',
+                  onClick: () => setStartDate(endDate.startOf('year')),
+                },
+              ]}
+            />
+            <DateInput
+              label='To'
+              value={endDate}
+              onChange={setEndDate}
+              validate={(endDate) => {
+                if (startDate.isAfter(endDate))
+                  return 'Start must be before end!';
+              }}
+              presets={[{ label: 'today', onClick: () => setEndDate(dayjs()) }]}
+            />
+          </LocalizationProvider>
+        </Stack>
+
         <Box
           sx={{
             display: 'flex',
